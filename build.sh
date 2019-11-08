56c79dfa9862320395bb1f8d2e6a03b7f9328cb5
Richard Wang <rwang83@gmail.com>
2019-11-08 01:04:21 -0500

    add files

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+config=$1
+
+git submodule foreach --recursive git clean -xfd
+git submodule foreach --recursive git reset --hard
+
+echo cp -r $config/* $DIR/Marlin
+cp -r $config/* $DIR/Marlin
+pwd=`pwd`
+cd $DIR/Marlin
+pio run -e `cat board.txt`
+cd $pwd
