cbec022d5f3eee6f2bb78cdc40f7e0ff0f5ce9a1
Richard Wang <rwang83@gmail.com>
2019-11-08 01:52:13 -0500

    i3plus build works

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 config=$1
 
 git submodule foreach --recursive git clean -xfd
-git submodule foreach --recursive git reset --hard
-git submodule update --init --recursive
+git submodule foreach --recursive git reset --hard 75efa3cdacd8271ca360e7d1680f25f5be218da3
+#git submodule update --init --recursive
 
 echo cp -r $config/* $DIR/Marlin
 cp -r $config/* $DIR/Marlin
