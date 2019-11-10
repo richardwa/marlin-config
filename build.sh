c504b6b293b96ffc8d82c7a3bd611191abcaf6bb
Richard Wang <rwang83@gmail.com>
2019-11-10 03:56:51 -0500

    upgrade marlin

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 config=$1
 
 git submodule foreach --recursive git clean -xfd
-git submodule foreach --recursive git reset --hard 75efa3cdacd8271ca360e7d1680f25f5be218da3
-#git submodule update --init --recursive
+git submodule foreach --recursive git reset --hard 4db68d704f560d8450bfb4d1ee3127b34623c319
 
 cp -r -p $config/* $DIR/Marlin
 cp -p $DIR/platformio.ini $DIR/Marlin
-cp -p $DIR/upload.py $DIR/Marlin
 
 pwd=`pwd`
 cd $DIR/Marlin
