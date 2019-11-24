78f7b3f3147515755f035099f01440c963d02a8e
Richard Wang <rwang83@gmail.com>
2019-11-23 22:30:52 -0500

    update and disable linear advance

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 config=$1
 
 git submodule foreach --recursive git clean -xfd
-git submodule foreach --recursive git reset --hard 4db68d704f560d8450bfb4d1ee3127b34623c319
+git submodule foreach --recursive git reset --hard 8f09d2e7904f8f743be149b6b213575e7f36ee29
 
 cp -r -p $config/* $DIR/Marlin
 
