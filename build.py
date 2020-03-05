8159ece8c1bacb180ace35f62068dd5f19f526e4
Richard Wang <rwang83@gmail.com>
2020-03-05 01:21:12 -0500

    update

--- build.py
+++ build.py
@@ -9,12 +9,15 @@ args = parser.parse_args()
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-os.system('git submodule Marlin clean -xfd')
+os.system('git submodule foreach --recursive git clean -xfd')
 os.system('git submodule foreach --recursive git reset --hard d6e767e36be5852a32526c08d9ade974b18f6546')
 os.system(
     f'xcopy /E /Y {os.path.join(DIR, args.config, "*")} {os.path.join(DIR, "Marlin")}')
 
 pwd = os.getcwd()
 os.chdir(os.path.join(DIR, "Marlin"))
+# for Windows add pio to path: %USERPROFILE%\.platformio\penv\Scripts
 os.system('pio run -e LPC1768')
 os.chdir(pwd)
+
+
